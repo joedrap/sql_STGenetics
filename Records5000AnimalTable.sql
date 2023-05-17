e63f57efb1083870b913ed520a649afc5caf9ac1
joedrap <133183313+joedrap@users.noreply.github.com>
2023-05-17 05:59:55 -0500

    Delete Records5000AnimalTable.sql

--- Records5000AnimalTable.sql
+++
@@ -1,9 +0,0 @@
-USE STGenetics
-
-DECLARE @i INT = 0
-WHILE @i < 5000
-BEGIN
-    INSERT INTO Animal (Name, Breed, BirthDate, Sex, Price, Status)
-    VALUES ('Name' + CAST(@i AS VARCHAR), 'Breed' + CAST(@i AS VARCHAR), '2000-01-01', 'Male', 1000.00, 'Active');
-    SET @i = @i + 1
-END
